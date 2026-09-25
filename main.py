3b6a00a8419775b80c64d7a726e100ccaa5cba7c
Santhosh-kumar2004 <sk2004santhoshkumar@gmail.com>
2026-09-25 19:41:26 +0530

    Updated

---
+++ main.py
@@ -0,0 +1,48 @@
+import os
+from dotenv import load_dotenv
+from ollama import Client
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+load_dotenv()
+OLLAMA_API_KEY = os.environ.get("OLLAMA_API_KEY")
+OLLAMA_MODEL = os.environ.get("OLLAMA_MODEL", "gpt-oss:120b")
+
+app=FastAPI()
+
+class Request(BaseModel):
+    user_input:str
+
+@app.post("/generate")
+def generate(input:Request):
+    reply=ask_ai(input.user_input)
+
+    return {
+    "response": reply
+}
+
+def get_client():
+    if not OLLAMA_API_KEY:
+        raise RuntimeError("OLLAMA_API_KEY is not set in your .env file")
+
+    return Client(
+        host="https://ollama.com",
+        headers={"Authorization": f"Bearer {OLLAMA_API_KEY}"},
+    )
+
+
+def ask_ai(user_input):
+    client = get_client()
+
+    response = client.chat(
+        model=OLLAMA_MODEL,
+        messages=[
+            {
+             "role":"user",
+            "content":user_input
+            }
+            ]
+    )
+
+    return response["message"]["content"]
